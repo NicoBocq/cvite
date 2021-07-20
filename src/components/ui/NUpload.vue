b2070e5a9ac127b85f6b98588619faa0c2ef33d0
NicoBocq <nicolas.fow@gmail.com>
2021-07-20 16:40:03 +0200

    Done: resume design and layout

---
+++ src/components/ui/NUpload.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="NUpload">
+        <pre v-text="$attrs"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Nupload'
+    };
+</script>
--- src/pdf.js
+++ src/pdf.js
@@ -2,52 +2,144 @@ import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs
 import { resume } from '/src/store'
-import {reactive, toRefs} from 'vue'
+
+// const fonts = {
+//   Inter : {
+//     normal: '/src/assets/fonts/Inter-Regular.ttf',
+//     semiBold: '/src/assets/fonts/Inter-SemiBold.ttf',
+//     bold: '/src/assets/fonts/Inter-ExtraBold.ttf',
+//   }
+// }
+
+const fonts = {
+  Helvetica: {
+    normal: 'Helvetica',
+    bold: 'Helvetica-Bold',
+    italics: 'Helvetica-Oblique',
+    bolditalics: 'Helvetica-BoldOblique'
+  },
+}
+const themeColor = '#6a737b'
 
 export function printToPdf () {
   const item = resume.value
-  const experiences = item.experiences.reduce((r, i) => {
+  const experienceList = item.experiences.reduce((r, i) => {
     r.push(
-      { text: i.year + ' - ' + i.title + '\n', style: 'listHeader' },
-      { text: i.company + '\n', style: 'listSubHeader'},
-      { text: i.description + '\n', style: 'desc' })
-    return r
+      {
+        stack: [
+          {text: i.title, style: 'listItemHeader'},
+          {text: i.year.toUpperCase(), style: 'listItemSubHeader'},
+          {text: i.description, style: 'listItemDesc' }
+        ], style: 'listItem'
+      })
+    return r.sort((a, b) => a.order - b.order)
+  }, [])
+  const educationList = item.education.reduce((r, i) => {
+    r.push(
+      { stack: [
+          { text: i.degree, style: 'listItemHeader' },
+          { text: i.beginDate + ' - ' + i.endDate + ' / ' + i.school + ' - ' + i.city, style: 'listItemSubHeader'},
+          { text: i.school + ' - ' + i.city, style: 'listItemSubHeader'},
+          { text: i.description, style: 'listItemDesc' }
+        ], style: 'listItem'
+      })
+    return r.sort((a, b) => a.order - b.order)
   }, [])
   const skillList = item.skills.reduce((r, i) => {
-    r.push(`${i}\n`)
+    r.push({ text: i, style: 'listItem' })
     return r
   }, [])
-  const skills = [{ text: 'Compétences\n', style: 'title'}, { ul: skillList, style: 'listItem' }]
+  const linkList = item.links.reduce((r, i) => {
+    r.push({ text: i.label, link: i.url, style: 'headerListLinks' })
+    return r
+  }, [])
+  const infosList = [
+    ...(item.email ? [{ text: item.email, style: 'headerListItem' }] : []),
+    ...(item.phone ? [{ text: item.phone, style: 'headerListItem' }] : []),
+    ...(item.address? [{ text: item.address, style: 'headerListItem' }] : []),
+  ]
+
   const header = [
-    { text: `${item.firstName} ${item.lastName}\n`, style: 'name' },
-    { text: item.summary, style: 'description' }
+    { stack : [
+      { text: `${item.firstName} ${item.lastName}`, style: 'name' },
+      { text: item.title, style: 'title' },
+      { text: item.summary, style: 'summary' }
+    ], style: 'headerMain'}
+  ]
+  const linksSection = [
+    { text: 'Liens', style: 'sectionTitle'},
+    { stack: linkList, style: 'list'}
+  ]
+  const infosSection = [
+    { stack: infosList, style: 'headerLinks'}
+  ]
+  const skillsSection = [
+    { text: 'Compétences', style: 'sectionTitle'},
+    { stack: skillList, style: 'list' }
+  ]
+  const educationSection = [
+    { text: 'Education', style: 'sectionTitle'},
+    { stack: educationList, style: 'list' }
+  ]
+  const experienceSection = [
+    { text: 'Expériences', style: 'sectionTitle'},
+    { stack: experienceList, style: 'list' },
+  ]
+  const headerSide = [
+    infosSection
   ]
   const main = [
-    { text: experiences, style: 'list' }
+    experienceSection,
+    educationSection
   ]
   const side = [
-    { text: 'info', style: 'list'},
-    { text: skills },
+    linksSection,
+    skillsSection
   ]
   const dd = {
+    // defaultStyle: {
+    //   font: 'Helvetica'
+    // },
+    background: function () {
+      return {
+        canvas: [
+          {
+            type: 'rect',
+            x: 395, y: 0, w: 200.28, h: 841.89,
+            color: themeColor
+          }
+        ]
+      };
+    },
     content: [
       {
-        text: header,
-        style: 'header'
+        columns: [
+          {
+            width: '70%',
+            stack: header,
+            style: 'headerMain'
+          },
+          {
+            width: '30%',
+            stack: headerSide,
+            style: 'headerSide'
+          }
+        ],
+        columnGap: 56
       },
       {
         columns: [
           {
             width: '70%',
-            text: main,
-            style: 'list'
+            stack: main
           },
           {
             width: '30%',
-            text: side
+            stack: side,
+            style: 'side'
           }
         ],
-        columnGap: 20
+        columnGap: 56
       }
     ],
     styles: {
@@ -56,44 +148,80 @@ export function printToPdf () {
         bold: true
       },
       list: {
-        margin: [0, 0, 0, 10],
-        border: true
+        margin: [0, 0, 0, 16],
       },
-      listItem: {
-        margin: [0, 0, 0, 5],
+      headerMain: {
+        margin: [0, 0, 0, 24],
       },
-      header: {
-        margin: [0, 0, 0, 20],
+      headerSide: {
+        fontSize: 10,
+        color: 'white',
+        margin: [0, 0, 0, 24]
       },
-      listHeader: {
-        fontSize: 12,
+      section: {
+        margin: [0, 0, 0, 24]
+      },
+      sectionTitle: {
+        fontSize: 16,
+        lineHeight: 1.1,
         bold: true,
-        margin: [0, 0, 0, 5]
-      },
-      title: {
-        fontSize: 14,
-        bold: true
+        margin: [0, 0, 0, 16]
       },
       main: {
 
       },
       side: {
-        background: '#e6e6e6'
+        color: 'white'
       },
       name: {
         fontSize: 16,
         bold: true,
+        margin: [0, 0, 0, 6],
+      },
+      title: {
+        color: themeColor,
+        fontSize: 18,
+        bold: true,
         margin: [0, 0, 0, 6]
       },
-      listSubHeader: {
+      summary: {
         fontSize: 10,
-        color: '#e6e6e6',
-        margin: [0, 0, 0, 5]
+        lineHeight: 1.1
       },
-      desc: {
-        fontSize: 12,
+      headerLinks: {
+        margin: [0, 0, 0, 6]
+      },
+      listItem: {
+        fontSize: 11,
+        margin: [0, 0, 0, 8]
+      },
+      listItemHeader: {
+        fontSize: 11,
+        // lineHeight: 1,
+        bold: true,
+        margin: [0, 0, 0, 3]
+      },
+      listItemSubHeader: {
+        fontSize: 10,
+        color: themeColor,
+        margin: [0, 0, 0, 3]
+      },
+      listItemDesc: {
+        lineHeight: 1.1,
+        fontSize: 10,
+      },
+      headerListLinks: {
+        decoration: 'underline',
+        margin: [0, 0, 0, 3],
+        lineHeight: 1.2,
+      },
+      headerListItem: {
+        lineHeight: 1.2,
+        margin: [0, 0, 0, 3],
       }
     }
   }
+  console.log(dd)
+  // pdfMake.createPdf(dd, null, fonts).open()
   pdfMake.createPdf(dd).open()
 }
