d8320728b0059cbe579c9c6d62784853933af05d
NicoBocq <nicolas.fow@gmail.com>
2021-10-12 15:30:08 +0200

    Build : add i18n

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  alias: {
+    'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
+  },
+  plugins: [
+    vue(),
+    vueI18n({
+      // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+      // compositionOnly: false,
+
+      // you need to set i18n resource including paths !
+      // include: path.resolve(__dirname, './path/to/src/locales/**')
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src')
