1195f05b97194b8951efcf67df6ec2b9eb0b8ca1
NicoBocq <nicolas.fow@gmail.com>
2021-10-15 17:53:44 +0200

    Fix build ?

--- vite.config.js
+++ vite.config.js
@@ -9,15 +9,15 @@ export default defineConfig({
     'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
   },
   plugins: [
-    vue(),
-    vueI18n({
-      include: path.resolve(__dirname, '/src/locales/**')
-      // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-      // compositionOnly: false,
-
-      // you need to set i18n resource including paths !
-      // include: path.resolve(__dirname, './path/to/src/locales/**')
-    })
+    vue()
+    // vueI18n({
+    //   include: path.resolve(__dirname, '/src/locales/**')
+    //   // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+    //   // compositionOnly: false,
+    //
+    //   // you need to set i18n resource including paths !
+    //   // include: path.resolve(__dirname, './path/to/src/locales/**')
+    // })
   ],
   resolve: {
     alias: {
