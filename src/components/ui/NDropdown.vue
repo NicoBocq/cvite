d39d1d973c355a4500e2dca3ed02ff1f76782e87
NicoBocq <nicolas.fow@gmail.com>
2021-10-15 17:36:02 +0200

    init dropdown component

--- src/components/ui/NDropdown.vue
+++ src/components/ui/NDropdown.vue
@@ -1,92 +1,117 @@
 <template>
-  <div class="relative inline-block text-left">
-    <menu>
-      <span class="rounded-md shadow-sm">
-        <menu-button
-          class="
-            inline-flex
-            justify-center
-            w-full
-            px-4
-            py-2
-            text-sm
-            font-medium
-            leading-5
-            text-gray-700
-            transition
-            duration-150
-            ease-in-out
-            bg-white
-            border border-gray-300
-            rounded-md
-            hover:text-gray-500
-            focus:outline-none focus:border-blue-300 focus:shadow-outline-blue
-            active:bg-gray-50 active:text-gray-800
-          "
+  <div class="w-auto text-right">
+    <Menu
+      as="div"
+      class="relative inline-block text-left"
+    >
+      <div>
+        <MenuButton
+          class="inline-flex justify-center text-brand-600 hover:text-brand-700 w-full px-4 py-1 text-sm font-medium rounded-lg focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
         >
-          More
-        </menu-button>
-      </span>
+          <n-icon
+            :icon="icon"
+            size="w-6 h-6"
+          />
+        </MenuButton>
+      </div>
+
       <transition
         enter-active-class="transition duration-100 ease-out"
         enter-from-class="transform scale-95 opacity-0"
         enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-out"
+        leave-active-class="transition duration-75 ease-in"
         leave-from-class="transform scale-100 opacity-100"
         leave-to-class="transform scale-95 opacity-0"
       >
-        <menu-items
-          class="
-            absolute
-            right-0
-            w-56
-            mt-2
-            origin-top-right
-            bg-white
-            border border-gray-200
-            divide-y divide-gray-100
-            rounded-md
-            shadow-lg
-            outline-none
-          "
+        <MenuItems
+          class="absolute right-0 w-56 mt-2 p-1 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
         >
-          <menu-item
-            v-slot="{ active }"
-            class="
-              flex
-              justify-between
-              w-full
-              px-4
-              py-2
-              text-sm
-              leading-5
-              text-left
-            "
-          >
-            <a :class="{ 'bg-blue-500': active }" href="/account-settings">
-              Account settings
-            </a>
-          </menu-item>
-        </menu-items>
+          <MenuItem v-slot="{ active }">
+            <button
+              class="space-x-2"
+              :class="[
+                active ? 'bg-brand-500 text-white' : 'text-gray-700',
+                'group flex rounded-md items-center w-full px-2 py-2 text-sm',
+              ]"
+              @click="setLocale"
+            >
+              <n-icon icon="globe" />
+              <span>{{ t('ui.switchLang') }}</span>
+            </button>
+          </MenuItem>
+          <MenuItem v-slot="{ active }">
+            <button
+              class="space-x-2"
+              :class="[
+                active ? 'bg-brand-500 text-white' : 'text-gray-700',
+                'group flex rounded-md items-center w-full px-2 py-2 text-sm',
+              ]"
+              @click="addNicoBocq"
+            >
+              <n-icon icon="question-mark-circle" />
+              <span>{{ t('ui.about') }}</span>
+            </button>
+          </MenuItem>
+          <MenuItem v-slot="{ active }">
+            <button
+              class="space-x-2"
+              :class="[
+                active ? 'bg-brand-500 text-white' : 'text-gray-700',
+                'group flex rounded-md items-center w-full px-2 py-2 text-sm',
+              ]"
+              @click="setNewResume"
+            >
+              <n-icon icon="refresh" />
+              <span>{{ t('ui.startOver') }}</span>
+            </button>
+          </MenuItem>
+        </menuitems>
       </transition>
-    </menu>
+    </Menu>
   </div>
 </template>
 
 <script>
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import NIcon from './NIcon.vue'
+import { setNewResume, addNicoBocq } from '../../modules/resumeStore'
+import { useI18n } from 'vue-i18n'
 
 export default {
   components: {
+    NIcon,
     Menu,
     MenuButton,
     MenuItems,
     MenuItem
   },
   props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: 'chevron-down'
+    },
     options: {
       type: Array,
       default: () => []
+    }
+  },
+  setup () {
+    const { t, locale } = useI18n()
+
+    const setLocale = () => {
+      locale.value = locale.value === 'en' ? 'fr' : 'en'
+      // document.querySelector('html').setAttribute('lang', lang)
+    }
+
+    return {
+      setNewResume,
+      addNicoBocq,
+      t,
+      setLocale
     }
   }
 }
