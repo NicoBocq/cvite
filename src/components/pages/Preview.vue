886f2e1663215754654316f432e5815b1377a869
Nicolas Bocquet <macbocq@MacBook-Pro-de-Nicolas.local>
2021-12-22 11:48:01 +0100

    Add fonts

--- src/components/pages/Preview.vue
+++ src/components/pages/Preview.vue
@@ -47,3 +47,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap');
+  body {
+    font-family: 'Inter', sans-serif;
+}
+</style>
