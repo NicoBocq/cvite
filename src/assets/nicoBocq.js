d4ff91224feee4d1382a9f72bfe336219b550858
NicoBocq <nicolas.fow@gmail.com>
2024-10-02 17:30:42 +0200

    Updated

--- src/assets/nicoBocq.js
+++ src/assets/nicoBocq.js
@@ -4,8 +4,7 @@ export const nicoBocq = {
   firstName: 'Nicolas',
   lastName: 'Bocquet',
   title: 'Front-End Engineer',
-  summary:
-    "",
+  summary: "My career has been dedicated to enhancing user experience by making information easily accessible and manageable.",
   email: 'nicolas.fow@gmail.com',
   phone: '06 21 02 31 65',
   address: '29 rue Wulfram Puget - 13008 Marseille',
@@ -33,75 +32,80 @@ export const nicoBocq = {
       id: 8,
       beginDate: '2022',
       endDate: '',
-      title: 'Développeur Full-Stack (React Native - NextJs)',
-      company: 'Inforca',
-      description: '- Développement d\'une application de course automobile (guidage routier, calcul de temps, gestion des participants, des classements...)  \n' +  'React Native (CLI)  \n' + '- Développement d\'un CRM  \n' + 'NextJs (app router), Prisma, Shadcn'
+      title: 'Full-Stack Developer (React Native - NextJs - Typescript)',
+      company: 'Inforca (web agency)',
+      description: `- IOS / Android app: regularity rally application (B2C):
+      Main features: road guidance, time calculation, participant management, rankings...
+      React Native (CLI), Redux Toolkit, Tailwind, OpenLayers
+      - Web app: Product Information Management (B2B - fashion)
+      Main features: +100k references, role base access, complex filtering, bulk edition, file import, test unit and e2e
+      NextJs 14 (app router), Prisma/PostgreSQL, Shadcn/Radix UI, Vitest / Playwright`
     },
     {
       id: 7,
       beginDate: '2022',
       endDate: '2022',
-      title: 'Développeur Front-End (React - Ecommerce)',
-      company: 'Alltricks',
-      description: 'Développement d\'un site Ecommerce et de son backoffice, consommation d\'API et mise en place du design system avec React, Redux et Typescript).'
+      title: 'Front-End Developer (React)',
+      company: 'Alltricks (e-commerce)',
+      description: 'Development of an Ecommerce site and its back office, API consumption, and implementation of the design system with React, Redux, and Typescript.'
     },
     {
       id: 6,
       beginDate: '2020 - 2021',
       endDate: '',
-      title: 'Développeur Front-End (Vue - B2B)',
-      company: 'Internethic',
+      title: 'Front-End Developer (Vue)',
+      company: 'Oparedo (CRM editor - B2B)',
       description:
-        "Développement d'une app BtoB (gestion de contrats, faturation...): \n" +
-        '- développement de composants (UI et custom) et de stores : Vuejs, Vuex, Tailwind, Storybook\n' +
-        '- spécifications fonctionnelles et ergonomiques\n' +
-        '- test unitaires'
+        "Development of a CRM app (contract management, invoicing...): \n" +
+        '- development of components (UI and custom) and stores: Vuejs, Vuex, Tailwind, Storybook\n' +
+        '- functional and ergonomic specifications\n' +
+        '- unit tests'
     },
     {
       id: 5,
       beginDate: '2019',
       endDate: '2020',
-      title: 'Développeur Front-End (Vue - B2C)',
-      company: 'Novazéo',
+      title: 'Front-End Developer (Vue)',
+      company: 'Novazéo (web agency - B2C)',
       description:
-        'Intégration et développement sur Django CMS / Symfony (VueJs, OpenLayers).' +
-        'Conception UX/UI (Figma).'
+        'Integration and development on Django CMS / Symfony (VueJs, OpenLayers).' +
+        'UX/UI design (Figma).'
     },
     {
       id: 4,
       beginDate: '2016',
       endDate: '2018',
-      title: 'Journaliste web',
+      title: 'Journalist',
       company: 'La Provence',
       description:
-        'Rédaction, production de modules éditoriaux (Data journalisme, infographies).'
+        'Writing, production of editorial modules (Data journalism, infographics).'
     },
     {
       id: 3,
       beginDate: '2011',
       endDate: '2015',
-      title: 'Chef de Projet numérique',
+      title: 'Product Owner',
       company: 'La Provence',
       description:
-        'En charge de projets numériques : web, mobiles, applications, réseaux sociaux...'
+        'In charge of digital projects: web, mobile, applications, social networks...'
     },
     {
       id: 2,
       beginDate: '2007',
       endDate: '2010',
-      title: 'Développeur web',
+      title: 'Web Developer',
       company: 'La Provence',
       description:
-        'Développement et intégration, accompagnement de la rédaction au numérique.'
+        'Development and integration, accompaniment of the writing team to digital.'
     },
     {
       id: 1,
       beginDate: '2001',
       endDate: '2007',
-      title: 'Développeur web',
+      title: 'Web Developer',
       company: 'Freelance',
       description:
-        'Création de sites web : graphisme, développement et intégration'
+        'Creation of websites: graphics, development, and integration'
     }
   ],
   education: [
@@ -110,7 +114,7 @@ export const nicoBocq = {
       beginDate: '2019',
       endDate: '',
       school: 'Le Wagon',
-      degree: 'Développeur Fullstack',
+      degree: 'Fullstack Developer',
       city: 'Marseille',
       description: 'Ruby, Ruby & Rails, React'
     },
@@ -119,7 +123,7 @@ export const nicoBocq = {
       beginDate: '1999',
       endDate: '2001',
       school: 'Université des Pays du Vaucluse',
-      degree: 'MST Communication : conception multimédias',
+      degree: 'Master Communication : conception multimédias',
       city: 'Avignon',
       description: ''
     }
@@ -136,17 +140,18 @@ export const nicoBocq = {
   skill: [
     { id: 1, label: 'Javascript / Typescript' },
     { id: 3, label: 'React / React Native / NextJs' },
-    { id: 11, label: 'Prisma / GraphQL'},
+    { id: 11, label: 'ORM: Prisma / Drizzle'},
+    { id: 12, label: 'PostgreSQL / MySQL'},
     { id: 2, label: 'Vue / Vuex' },
     { id: 4, label: 'Tailwind, Shadcn, Radix, Material UI, Headless UI' },
     { id: 5, label: 'Node, Express' },
-    { id: 6, label: 'D3, OpenLayers' },
-    { id: 7, label: 'Figma, Adobe XD' },
+    { id: 6, label: 'D3, OpenLayers, Mapbox' },
+    { id: 7, label: 'Figma, Adobe XD, Storybook' },
     {
       id: 9,
-      label: 'Tests unitaires et fonctionnels : React Testing, Behat, Gherkin, Cypress'
+      label: 'Unit tests and functional tests: React Testing Library, Vitest, Playwright, Behat, Gherkin, Cypress'
     },
-    { id: 10, label: 'Anglais'}
+    { id: 10, label: 'English'}
   ],
-  hobby: 'Side projects, Histoire, sports (course à pied, football)'
+  hobby: 'Side projects, History, doing sport (running)'
 }
